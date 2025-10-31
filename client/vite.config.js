dc97c01c4a4ad7c9852fe5dfce4708fbb53f5c3f
Kostya Umsha <umsha.kostya@webmaestro.com.ua>
2025-10-31 22:22:02 +0200

    added vite base

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    base: '/rokich-test/',
     plugins: [react(), tailwindcss()],
     server: {
         proxy: {
